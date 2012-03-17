cc487564d67957fa57ab61a119f624927b6795ec
Andrei Maxim <andrei@andreimaxim.ro>
2012-03-17 13:23:51 +0200

    Afisam intrebari pe pagina de index

--- miniflow.rb
+++ miniflow.rb
@@ -1,8 +1,25 @@
+require "ostruct"
+
 require "bundler/setup"
 Bundler.require :default, ENV["RACK_ENV"]
 
 class Miniflow < Sinatra::Base
+  enable :static
+
+  configure do
+    set :questions, []
+
+    question = OpenStruct.new
+    question.score = 128
+    question.title = "O intrebare de Ruby"
+    question.category = "ruby"
+    question.created_at = Time.now
+
+    settings.questions << question
+  end
+
   get "/" do
-    "Hello, world"
+    @questions = settings.questions
+    erb :index
   end
 end
